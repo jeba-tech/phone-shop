4df6e9e246a266cf86b2a1d4912f47bb041abd4e
JEBA <fawjiajeba@gmail.com>
2022-03-07 13:18:18 +0600

    detail added

--- phone.js
+++ phone.js
@@ -44,12 +44,12 @@ const displaySearchResult = data => {
             const div = document.createElement('div');
             div.classList.add('col');
             div.innerHTML = `
-        <div onclick="loadMealDetail(${meal.data})" class="card h-100">
+        <div class="card h-100">
             <img src="${meal.image}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${meal.phone_name}</h5>
                 <h6 class="card-subtitle mb-2 text-muted">${meal.brand}</h6>
-                <p class="card-text"><a href="#" class="btn btn-outline-dark">More Details</a></p>
+                <p><a onclick="loadMealDetail(${meal.data})" class="btn btn-outline-dark">More Details</a></p>
                 
             </div>
         </div>
@@ -59,23 +59,28 @@ const displaySearchResult = data => {
 }
 
 const loadMealDetail = mealId => {
-      const url = `https://openapi.programming-hero.com/api/phone/${slug}`;
+      const url = `https://openapi.programming-hero.com/api/phone/apple_iphone_13_pro_max-11089`;
       fetch(url)
             .then(res => res.json())
-            .then(data => displayMealDetail(data.data[0]));
+            .then(data => displayMealDetail(data.data));
 }
 
 const displayMealDetail = meal => {
-      console.log(meal);
+      // console.log(meal);
       const mealDetails = document.getElementById('meal-details');
       const div = document.createElement('div');
       div.classList.add('card');
       div.innerHTML = `
-    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
-    <div class="card-body">
-        <h5 class="card-title">${meal.strMeal}</h5>
-        <p class="card-text">${meal.strInstructions.slice(0, 150)}</p>
-        <a href="${meal.strYoutube}" class="btn btn-primary">Go somewhere</a>
+   
+    <div class="card-body ">
+    <img src="${meal.image}" class="card-img-top my-2 " alt="...">
+        <h5 class="card-title">${meal.name}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">${meal.brand}</h6>
+        <p class="card-text">${meal.slug}</p>
+        <p class="card-text"><b>Sensors:</b> ${meal.mainFeatures.sensors}</p>
+        <p class="card-text"><b>Release Date:</b> ${meal.releaseDate}</p>
+        <p class="card-text"><b>Bluetooth:</b> ${meal.others.Bluetooth}</p>
+       
     </div>
     `;
       mealDetails.appendChild(div);
